49dbeb5001c7cd526c7135bf9a8400bedc385d6f
Liam Bohl <liambohl@gmail.com>
2017-10-13 13:46:30 -0400

    Add project files.

---
+++ SymmetryExplorer/SymmetryExplorer.h
@@ -0,0 +1,35 @@
+
+// SymmetryExplorer.h : main header file for the SymmetryExplorer application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CSymmetryExplorerApp:
+// See SymmetryExplorer.cpp for the implementation of this class
+//
+
+class CSymmetryExplorerApp : public CWinApp
+{
+public:
+	CSymmetryExplorerApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSymmetryExplorerApp theApp;
